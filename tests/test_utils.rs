eea71bfd32e47c64b3a8c20047ddbdde70dba8ef
David Bonet <webbonet@gmail.com>
2019-11-17 17:52:11 +0100

    refactor tests

---
+++ tests/test_utils.rs
@@ -0,0 +1,84 @@
+use std::fs::{remove_dir_all, create_dir_all};
+use std::path::PathBuf;
+use std::io::ErrorKind::NotFound;
+
+pub mod tests {
+    pub struct TestCleaner<S = Box<dyn Fn(&str) -> ()>, T = Box<dyn Fn(&str) -> ()>>
+        where S: Fn(&str) -> (),
+              T: Fn(&str) -> ()
+    {
+        setup: S,
+        teardown: T,
+    }
+
+    impl TestCleaner {
+        pub fn new<S: 'static, T: 'static>(setup: S, teardown: T) -> Self
+            where S: Fn(&str) -> (),
+                  T: Fn(&str) -> ()
+        {
+            TestCleaner { 
+                setup: Box::new(setup),
+                teardown: Box::new(teardown),
+            }
+        }
+
+        pub fn run<R>(&self, test: R)  
+            where R: FnOnce(&str) -> () + std::panic::UnwindSafe
+        {
+            let folder_name = super::random_name();
+
+            (self.setup)(folder_name.as_ref());
+
+            let result = std::panic::catch_unwind(|| {
+                test(folder_name.as_ref())
+            });
+
+            (self.teardown)(folder_name.as_ref());
+
+            result.unwrap();
+        }
+    }
+}
+
+fn random_name() -> String {
+    use std::iter;
+    use rand::{Rng, thread_rng};
+    use rand::distributions::Alphanumeric;
+     
+    let mut rng = thread_rng();
+    iter::repeat(())
+            .map(|()| rng.sample(Alphanumeric))
+            .take(7)
+            .collect::<String>()
+}
+
+fn delete_temp_dir(folder_name: &str){
+    let path = PathBuf::from(format!("./temp/{}", folder_name));
+    let result = remove_dir_all(&path);
+    
+    println!("Deleting files... {:?} - {:?}", path, result);
+
+    match result {
+        Err(ref e) if e.kind() == NotFound => {},
+        Err(ref e) if e.kind() != NotFound => result.unwrap(),
+        _ => {},
+    }
+}
+
+pub fn build_basic_runner() -> tests::TestCleaner {
+    tests::TestCleaner::new(|folder_name| {
+        delete_temp_dir(folder_name);
+        create_dir_all(format!("./temp/{}", folder_name)).unwrap();
+    }, |folder_name| {
+        delete_temp_dir(folder_name);
+    })
+}
+
+/*#[test]
+fn test_test_unitilities() {
+    let runner = build_basic_runner();
+
+    runner.run(|_folder_name| {
+        panic!();
+    })
+}*/
--- tests/unzip_file.rs
+++ tests/unzip_file.rs
@@ -1,4 +1,4 @@
-use std::fs::{remove_dir_all, File};
+use std::fs::File;
 use std::{io, path::Path};
 use async_std::task;
 
@@ -6,60 +6,68 @@ use sha2::{digest::Digest, Sha256};
 
 use rzip::unzip_archive;
 
+mod test_utils;
+
+use test_utils::build_basic_runner;
+
 #[test]
 fn test_unzip() {
-    task::block_on(async {
-        let _ = unzip_archive(
-            File::open("tests/dummy_files/zip_10MB.zip").expect("The dummy zip file was not found"),
-            Path::new("tests/temp"),
-        )
-        .await;
+    let runner = build_basic_runner();
 
-        let files = vec![
-            (
-                "tests/temp/zip_10MB/file-example_PDF_1MB.pdf",
-                "5E4D40FCD8B22453A5DA2D32533B128F2565F3FC7A4D1647A93C86CDBB4BE37A",
-            ),
-            (
-                "tests/temp/zip_10MB/file_example_JPG_1MB.jpg",
-                "683A8528125CA09D8314435C051331DE2B4C981C756721A2D12C103E8603A1D2",
-            ),
-            (
-                "tests/temp/zip_10MB/file_example_ODS_5000.ods",
-                "2DD8CA392783A86AFC0B0120B333C7FF83BB5FCFB1A0088BE6D5957E05DA1C91",
-            ),
-            (
-                "tests/temp/zip_10MB/file_example_PNG_2500kB.jpg",
-                "7AB84F2D1A5806C3A0BCE9BC67FEDA52F55423950656C50B7C07733CEB6F956A",
-            ),
-            (
-                "tests/temp/zip_10MB/file_example_PPT_1MB.ppt",
-                "B709DEBB365A5437F2472F350745ED2F8A6890D7CB3D81E6750F2D5DD44625C9",
-            ),
-            (
-                "tests/temp/zip_10MB/file_example_TIFF_10MB.tiff",
-                "7D931EB0A8B51EDC594A255739785C6B297D081B27A91BF942B6475BC322596D",
-            ),
-            (
-                "tests/temp/zip_10MB/file-sample_1MB.doc",
-                "C560136E2A2B7036523F69EFDB4E9CDF369ABE167BA3A095E26D74E261774B20",
-            ),
-        ];
+    runner.run(|folder_name| {
+        task::block_on(async {
+            let _ = unzip_archive(
+                File::open("tests/dummy_files/zip_10MB.zip").expect("The dummy zip file was not found"),
+                Path::new(&format!("tests/temp/{}", folder_name)),
+            )
+            .await;
 
-        for (file_name, expected_hash) in files {
-            let hash = hash_file(file_name)
-                .await
-                .unwrap_or_else(|_| panic!("Couldn't hash file {}", file_name));
-            assert_eq!(expected_hash, hash);
-        }
+            let files = vec![
+                (
+                    format!("tests/temp/{}/zip_10MB/file-example_PDF_1MB.pdf", folder_name),
+                    "5E4D40FCD8B22453A5DA2D32533B128F2565F3FC7A4D1647A93C86CDBB4BE37A",
+                ),
+                (
+                    format!("tests/temp/{}/zip_10MB/file_example_JPG_1MB.jpg", folder_name),
+                    "683A8528125CA09D8314435C051331DE2B4C981C756721A2D12C103E8603A1D2",
+                ),
+                (
+                    format!("tests/temp/{}/zip_10MB/file_example_ODS_5000.ods", folder_name),
+                    "2DD8CA392783A86AFC0B0120B333C7FF83BB5FCFB1A0088BE6D5957E05DA1C91",
+                ),
+                (
+                    format!("tests/temp/{}/zip_10MB/file_example_PNG_2500kB.jpg", folder_name),
+                    "7AB84F2D1A5806C3A0BCE9BC67FEDA52F55423950656C50B7C07733CEB6F956A",
+                ),
+                (
+                    format!("tests/temp/{}/zip_10MB/file_example_PPT_1MB.ppt", folder_name),
+                    "B709DEBB365A5437F2472F350745ED2F8A6890D7CB3D81E6750F2D5DD44625C9",
+                ),
+                (
+                    format!("tests/temp/{}/zip_10MB/file_example_TIFF_10MB.tiff", folder_name),
+                    "7D931EB0A8B51EDC594A255739785C6B297D081B27A91BF942B6475BC322596D",
+                ),
+                (
+                    format!("tests/temp/{}/zip_10MB/file-sample_1MB.doc", folder_name),
+                    "C560136E2A2B7036523F69EFDB4E9CDF369ABE167BA3A095E26D74E261774B20",
+                ),
+            ];
 
-        remove_dir_all("tests/temp/zip_10MB").expect("The unzipped directory couldn't be deleted");
-    })
+            for (file_name, expected_hash) in files {
+                let hash = hash_file(&file_name)
+                    .await
+                    .unwrap_or_else(|_| panic!("Couldn't hash file {}", file_name));
+                assert_eq!(expected_hash, hash);
+            }
+        });
+    });
+
 }
 
 async fn hash_file(file: &str) -> Result<String, io::Error> {
     let mut file = File::open(file)?;
     let mut hasher = Sha256::new();
-    let _ = io::copy(&mut file, &mut hasher);
+    io::copy(&mut file, &mut hasher).unwrap();
+    drop(file);
     Ok(format!("{:X}", hasher.result()))
 }
