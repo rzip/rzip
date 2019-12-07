8b872c5de4f22dc7d251dbb53bd844117206af1d
David Bonet <webbonet@gmail.com>
2019-12-07 17:14:35 +0100

    split error types

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use async_std::fs::{create_dir_all, set_permissions, Permissions};
 use std::{
-    error::Error,
     fs::File,
     io,
     path::{Path, PathBuf},
@@ -11,31 +10,35 @@ use zip::result::ZipError;
 #[derive(Debug)]
 pub struct FileUnzipResult {
     pub name: Option<String>,
-    pub result: Result<(), UnzipError>,
+    pub result: Result<(), UnzipFileError>,
 }
 
 #[derive(Debug)]
-pub enum UnzipError {
+pub enum UnzipFileError {
     OtherError,
     ZipError(ZipError),
+}
+
+#[derive(Debug)]
+pub enum UnzipArchiveError {
+    FileReadingError(ZipError),
+    ArchiveReadingError(ZipError),
 }
 
 pub async fn unzip_archive(
     file: File,
     directory: &Path,
-) -> Result<Vec<FileUnzipResult>, Box<dyn Error>> {
-    let mut archive = zip::ZipArchive::new(file)?;
+) -> Result<Vec<FileUnzipResult>, UnzipArchiveError> {
+    let mut archive = match zip::ZipArchive::new(file) {
+        Ok(result) => result,
+        Err(error) => return Err(UnzipArchiveError::ArchiveReadingError(error)),
+    };
     let mut results = vec![];
 
     for file_index in 0..archive.len() {
         match archive.by_index(file_index) {
             Ok(file) => results.push(unzip_file(file, directory).await),
-            Err(error) => {
-                results.push(FileUnzipResult {
-                    name: None,
-                    result: Err(UnzipError::ZipError(error)),
-                });
-            }
+            Err(error) => return Err(UnzipArchiveError::FileReadingError(error)),
         };
     }
 
@@ -64,7 +67,7 @@ async fn unzip_file(mut file: ZipFile<'_>, directory: &Path) -> FileUnzipResult 
     FileUnzipResult {
         name: Some(file.name().to_owned()),
         //Still empty as the unzip_file function is full of unwrap and doesn't return errors
-        result: Err(UnzipError::OtherError),
+        result: Ok(()),
     }
 }
 
